9ab176b5f962e7aaebf5e4f2623b549ede83fbe6
Aca <aca.pikic@gmail.com>
2026-02-05 14:27:01 +0100

    Api rute za dodate fje

---
+++ quickbite-be/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Admin
+    Route::get('/users', [UserController::class, 'index']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    
+});
